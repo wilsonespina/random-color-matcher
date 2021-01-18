6723687d3624f590bc595e326478e57df589b005
Wilson Espina <wilson.espina@itv.com>
2021-01-18 20:27:10 +0000

    Add animations and css variables

--- dist/public.e31bb0bc.js
+++ dist/public.e31bb0bc.js
@@ -800,7 +800,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49468" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54939" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
